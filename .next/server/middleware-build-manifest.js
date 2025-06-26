fa2dc22889dbf80e06f72aac0ea6e3004343fa38
Meenu Parames <meenuparames@gmail.com>
2025-06-26 13:34:16 +0530

    Initial full project commit

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,22 @@
-globalThis.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-64f025a404122cab.js","static/chunks/4bd1b696-fd42d7f5bb41629d.js","static/chunks/684-1b24909a551b9093.js","static/chunks/main-app-ccb223b48ad337ff.js"],rootMainFilesTree:{},pages:{"/_app":["static/chunks/webpack-64f025a404122cab.js","static/chunks/framework-52167c1502110d79.js","static/chunks/main-e5310e75df531fe0.js","static/chunks/pages/_app-6635a3a4af8739be.js"],"/_error":["static/chunks/webpack-64f025a404122cab.js","static/chunks/framework-52167c1502110d79.js","static/chunks/main-e5310e75df531fe0.js","static/chunks/pages/_error-da855ac378341690.js"]},ampFirstPages:[]},globalThis.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+globalThis.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "rootMainFilesTree": {},
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
